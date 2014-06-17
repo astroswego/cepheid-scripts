15dcb22a155491bef3ade869fb831671f4e95f84
Rosnec <rosnec.dev@gmail.com>
2014-06-17 17:44:49 +0530

    improving axis labels

--- R/pl.r
+++ R/pl.r
@@ -64,7 +64,7 @@ formula_string <- function(ystr, ...) {
     paste(ystr, rhs, sep = " ~ ")
 }
 
-do_fit <- function(outdir, fname, data, params, ystr, ...) {
+do_fit <- function(outdir, fname, data, params, labels, ystr, ...) {
     # formula from string representation of equation
     eqn <- as.formula(formula_string(ystr, ...))
 
@@ -74,14 +74,18 @@ do_fit <- function(outdir, fname, data, params, ystr, ...) {
     # 2D plot
     fpath <- file.path(outdir, fname)
     png(fpath)
-    plot(data$logP, data$mag)
+    plot(data$logP, data$mag,
+         xlab = labels[[1]], ylab = labels[[2]])
     points(data$logP, model$fitted.values, col="red")
     dev.off()
     if (length(params) == 2) {
         # 3D plot
         fpath3d <- file.path(outdir, paste("3D", fname, sep = "-"))
         png(fpath3d)
-        s3d <- scatterplot3d(data$logP, params[[2]], data$mag)
+        s3d <- scatterplot3d(data$logP, params[[2]], data$mag,
+                             xlab = labels[[1]],
+                             ylab = labels[[3]],
+                             zlab = labels[[2]])
         s3d$plane3d(model)
         dev.off()
     }
@@ -94,17 +98,24 @@ do_fits <- function(outdir, datastr, data, tag) {
     PC2str <- paste(datastr, "PC2", sep = "$")
     
     do_fit(outdir,
-           paste("PL-", tag, ".png", sep = ""),
-           data, list(data$logP), magstr, logPstr)
+           paste("PL-", tag, ".png", sep = ""), data,
+           list(data$logP),
+           list("logP", "A0"),
+           magstr, logPstr)
     do_fit(outdir,
-           paste("PLPC1-", tag, ".png", sep = ""),
-           data, list(data$logP, data$PC1), magstr, logPstr, PC1str)
+           paste("PLPC1-", tag, ".png", sep = ""), data,
+           list(data$logP, data$PC1),
+           list("logP", "A0", "PC1"),
+           magstr, logPstr, PC1str)
     do_fit(outdir,
-           paste("PLPC2-", tag, ".png", sep = ""),
-           data, list(data$logP, data$PC2), magstr, logPstr, PC2str)
+           paste("PLPC2-", tag, ".png", sep = ""), data,
+           list(data$logP, data$PC2),
+           list("logP", "A0", "PC2"),
+           magstr, logPstr, PC2str)
     do_fit(outdir,
-           paste("PLPC12-", tag, ".png", sep = ""),
-           data, list(data$logP, data$PC1, data$PC2),
+           paste("PLPC12-", tag, ".png", sep = ""), data,
+           list(data$logP, data$PC1, data$PC2),
+           list("logP", "A0"),
            magstr, logPstr, PC1str, PC2str)
 }
 
@@ -119,11 +130,15 @@ split_data <- split_period(data, split = opts$period_split)
 low_data <- split_data[[1]]
 high_data <- split_data[[2]]
 
-cat("All logP")
+cat("All logP\n-----------------\n")
 do_fits(opts$output, "data", data, "all")
 
-cat(paste("logP < ", opts$period_split, "\n", sep = ""))
+cat(paste("logP < ", opts$period_split,
+          "\n-----------------\n", 
+          sep = ""))
 do_fits(opts$output, "low_data", low_data, "low")
 
-cat(paste("logP >= ", opts$period_split), "\n", sep = "")
+cat(paste("logP >= ", opts$period_split),
+    "\n-----------------\n",
+    sep = "")
 do_fits(opts$output, "high_data", high_data, "high")
